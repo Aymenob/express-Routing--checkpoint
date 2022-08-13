18cbb34bb56cd8c2e74012cd338f0bb73c36d2b7
Aymenob <aymenobba@gmail.com>
2022-08-13 13:42:57 +0200

    express-Routing

--- middlewares/availability.js
+++ middlewares/availability.js
@@ -2,7 +2,7 @@ let today = new Date();
 let day=today.getDay();
 let hour=today.getHours();
 function availability(req,res,next) {
-    if( hour<9|hour>17|day>5){
+    if( hour<=9|hour>=17|day>=5){
         res.send("<h1>website is closed at the moment!<h1/>")
     }  
     else
